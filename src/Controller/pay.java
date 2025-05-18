8cec6be194e5b2317d535d390fcbd61b32308d59
rhytham63 <rhythamacharya123@gmail.com>
2025-05-18 16:36:36 +0545

    Folder created

---
+++ src/Controller/pay.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pay;
+
+/**
+ *
+ * @author NITRO
+ */
+public class java {
+    
+}
---
+++ src/paygarum_groupe_37b/PayGarum_GroupE_37B.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paygarum_groupe_37b;
+
+/**
+ *
+ * @author NITRO
+ */
+public class PayGarum_GroupE_37B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
