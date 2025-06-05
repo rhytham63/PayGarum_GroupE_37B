2fd69a3cd871adb6b4f1f4d02a9d96a7ad61d602
rhytham63 <rhythamacharya123@gmail.com>
2025-06-05 12:26:34 +0545

    Small changes

---
+++ src/Model/fundT.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author NITRO
+ */
+
+public class fundT {
+    private double amountField;
+    private String recipientAccount;
+    // Getters and Setters
+    public double getAmountField() { 
+        return amountField; 
+    }
+    public void setAmountField(double amountField) { 
+        this.amountField = amountField; 
+    }
+    
+    public String getrecepientAccount() { 
+        return recipientAccount; 
+    }
+    public void setrecepientAccount(String recipientAccount) { 
+        this.recipientAccount = recipientAccount; 
+    }
+}
+
